0e8074ef415b523047b35c46b3fe636e37daafe3
ZhaurovVlad <vladzhaurov2004@gmail.com>
2023-06-16 20:25:19 +0900

    Первое задание 'Основные понятия'

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  	let arr = [];
+  	let d = Math.pow(b, 2) - (4 * a * c);
+
+	if (d < 0) {
+		arr = [];
+	}
+	else if (d == 0) {
+		arr.push(-b/(2*a));
+	}
+	else {
+		arr.push((-b + Math.sqrt(d) )/(2*a));
+		arr.push((-b - Math.sqrt(d) )/(2*a));
+	}
+  	
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent = percent*0.01/12;
+	let creditBody = amount - contribution;
+	let payment = creditBody * (percent	+ (percent / (Math.pow(1 + percent, countMonths) - 1)));
+	let allPrice = +(payment * countMonths).toFixed(2);
+
+	return allPrice;
 }
